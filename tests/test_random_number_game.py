c852252988d3243c75f528cc05d50037e1f2b9a2
Anthony <@home>
2017-02-09 13:35:19 -0600

    Got some testing working

--- tests/test_random_number_game.py
+++ tests/test_random_number_game.py
@@ -7,12 +7,24 @@ class TestGuessing(unittest.TestCase):
     # self.assertEqual(5, user_guessing(5))
     # @patch('builtins.input', return_value=5)
     # @patch('random_number_game.random.randrange', return=5)
-    @patch('random_number_game.get_random_number', return_value=5)
-    @patch('random_number_game.get_input', return_value=5)
-    def test_complete_game(self, mock_randrange, mock_input):
-        random_number_game.main()
+    # @patch('random_number_game.get_random_number()', side_effect=[5])
+    # def test_complete_game(self, mock_randint, mock_input):
+    #     self.assertEqual(random_number_game.main(), 'Good guess, you got it! The number was: 5')
 
-    @patch('random_number_game.get_input', side_effect=[5, 6, 7])
-    @patch('random_number_game.get_random_number', return_value=6)
+
+
+    # 'random_number_game.get_input', side_effect=[5]
+
+    # Test getting user input
+    @patch('builtins.input', side_effect=[5])
+    def test_input(self, mock_input):
+        self.assertEqual(random_number_game.get_input(), 5)
+
+    # Test Main function
+    @patch('random_number_game.get_input', side_effect=[6])
+    @patch('random_number_game.get_random_number', side_effect=[6])
     def test_complete_game(self, mock_randrange, mock_input):
-            random_number_game.main()
+        # random_number = random_number_game.get_random_number
+        # random_number_game.get_random_number = Mock()
+        random_number_game.main()
+        self.assertEqual(random_number_game.main(),'Good guess, you got it! The number was: 6' )
